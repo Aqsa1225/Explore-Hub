8d69670717631fec544c25c814ee9beb67cae255
AQSA RIAZ <aqsabathspa.ae@gmail.com>
2025-06-01 15:14:08 +0400

    update

--- models/favorite.js
+++ models/favorite.js
@@ -1,13 +1,15 @@
+// Import Mongoose to define schemas and interact with MongoDB
 const mongoose = require('mongoose');
 
 // Define the schema for the favorite images
 const favoriteSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  title: String,
-  imgUrl: String,
-  desc: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // Reference to the user who liked the image
+  title: String, // Title or name of the image
+  imgUrl: String, // Image URL
+  desc: String, // Optional description for the image
 });
 
+// Create the Favorite model using the schema
 const Favorite = mongoose.model('Favorite', favoriteSchema);
 
 module.exports = Favorite; // This exports the Favorite model
